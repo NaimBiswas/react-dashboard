589ffe47c5b1ad471616167972960bf3bafab307
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-18 00:38:24 +0600

    remove all css code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
    return (
       <div className="App">
-         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-
-            <h2>Start Creating 👉 React✡✡✡  Redux Dashboard🌟✡</h2>
-         </header>
+         <Header />
       </div>
    );
 }
