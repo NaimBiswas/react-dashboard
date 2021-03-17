7dc9f764a4d9fb678fed36f5cbc06ae62915f19c
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-18 00:39:36 +0600

    create a left sidebar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import LeftSideBar from './components/leftSideBar/LeftSideBar';
 
 function App() {
    return (
       <div className="App">
          <Header />
+         <LeftSideBar />
       </div>
    );
 }
